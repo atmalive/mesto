1849340a2dcb7f5c244ec6408a8397d544787948
Popov Mikhail <atmalive@gmail.com>
2022-05-01 11:09:28 +0200

    fix: scripts

--- scripts/script.js
+++ scripts/script.js
@@ -5,27 +5,24 @@ const nameInput = document.querySelector('.form__input-name');
 const whoInput = document.querySelector('.form__input-who');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-const submitButton = document.querySelector('.form__button');
 
 function openPop() {
-    event.preventDefault();
         nameInput.value = profileTitle.textContent;
         whoInput.value = profileSubtitle.textContent;
         form.classList.add('form_opened');
 }
 
 function ClosePop() {
-    event.preventDefault();
         form.classList.remove('form_opened');
 }
 
-function CloseOpacity() {
-    if (event.target === event.currentTarget) {
-        ClosePop();
-    }
-}
+// function CloseOpacity() {
+//     if (event.target === event.currentTarget) {
+//         ClosePop();
+//     }
+// }
 
-function submitChange() {
+function submitChange(event) {
     event.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = whoInput.value;
@@ -34,5 +31,5 @@ function submitChange() {
 
 buttonEdit.addEventListener('click', openPop);
 formCloseButton.addEventListener('click', ClosePop);
-form.addEventListener('click', CloseOpacity);
-submitButton.addEventListener('click', submitChange);
+// form.addEventListener('click', CloseOpacity);
+form.addEventListener('submit', submitChange);
