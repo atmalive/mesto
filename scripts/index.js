79305c47a77705eb57f0aadbc4cb9c93780fe06c
Popov Mikhail <atmalive@gmail.com>
2022-05-11 17:13:54 +0200

    feature: js universal openClose popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,48 @@
 const buttonEdit = document.querySelector(".profile__edit-button");
-const popup = document.querySelector(".popup");
-const popupCloseButton = document.querySelector(".popup__close-button");
+const buttonAdd = document.querySelector(".profile__add-button");
+
+const popupInfo = document.querySelector(".popup_type_info");
+const popupMesto = document.querySelector(".popup_type_mesto");
+const popupImg = document.querySelector(".popup_type_img");
+
 const popInputName = document.querySelector('.popup__input_type_name');
 const popInputJob = document.querySelector('.popup__input_type_job');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const popupForm = document.querySelector('.popup__inputs');
 
-function openPop() {
+// Закрытие на крестик
+function ClosePop(clPopup) {
+        clPopup.classList.remove('popup_open');
+}
+// Закрытие на фон
+function CloseOpacity(popup) {
+        if (event.target === event.currentTarget) {
+            ClosePop(popup);
+        }
+   }
+// открытие popup на кнопку addbutton
+function openPopAdd(openPopupAll) {
+        openPopupAll.classList.add('popup_open');
+        const popupCloseButton = openPopupAll.querySelector(".popup__close-button");
+        popupCloseButton.addEventListener('click',()=> ClosePop(openPopupAll));
+        openPopupAll.addEventListener('click',()=> CloseOpacity( openPopupAll));
+}
+
+function openInputInfo() {
         popInputName.value = profileTitle.textContent;
         popInputJob.value = profileSubtitle.textContent;
-        popup.classList.add('popup_open');
+        openPopAdd(popupInfo);
 }
-
-function ClosePop() {
-        popup.classList.remove('popup_open');
-}
-
-// function CloseOpacity() {
-//     if (event.target === event.currentTarget) {
-//         ClosePop();
-//     }
-// }
-
+// сохранение инпутов
 function submitChange(event) {
     event.preventDefault();
     profileTitle.textContent = popInputName.value;
     profileSubtitle.textContent = popInputJob.value;
-    ClosePop();
+    ClosePop(popupInfo);
 }
 
-buttonEdit.addEventListener('click', openPop);
-popupCloseButton.addEventListener('click', ClosePop);
-// form.addEventListener('click', CloseOpacity);
+buttonEdit.addEventListener('click', openInputInfo);
+buttonAdd.addEventListener('click',()=> openPopAdd(popupMesto));
+buttonEdit.addEventListener('click', openInputInfo);
 popupForm.addEventListener('submit', submitChange);
